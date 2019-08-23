a51ae831bf062508eb4acc49b2c5bf560906537b
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2019-08-23 22:08:47 +0300

    add solution for 1-module 1-task

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,14 @@
 function sum(a, b) {
-  /* ваш код */
+  var res = 0;
+  var res = 0;
+  if (Number(a) === a && Number(b) === b) {
+    res = a + b
+    return res;
+  }
+  else {
+    console.log('TypeError');
+    throw new TypeError('not valid params');
+  }
 }
 
 module.exports = sum;
